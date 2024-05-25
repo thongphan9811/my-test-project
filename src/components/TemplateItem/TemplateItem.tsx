f3a1527459b50c684e2e92ff9efea0da4cda3e86
Ngo Phan Thong <phanthong.1106@gmail.com>
2024-05-25 12:40:42 +0700

    chore: add logo and overlay for item

--- src/components/TemplateItem/TemplateItem.tsx
+++ src/components/TemplateItem/TemplateItem.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import classes from './TemplateItem.module.css';
 import { Link } from '@tanstack/react-router';
 
-type Props = React.ComponentProps<'img'> & { templateId: number; key: string };
+type Props = React.ComponentProps<'img'> & { templateId: number; title: string };
 
-export function TemplateItem({ src, alt = 'template', ...rest }: Props): React.ReactElement {
-  const url = new URL(window.location.href);
-  url.pathname = '/detail';
-  url.search = new URLSearchParams({
-    templateId: rest.templateId.toString(),
-  }).toString();
-
+export function TemplateItem({ src, alt = 'template', title, ...rest }: Props): React.ReactElement {
   return (
     <div className={classes['img-wrapper']} id={`parent-${rest.key}`}>
-      <Link to={url.href}>
+      <Link to={'/detail'} search={{ templateId: rest.templateId }} className={classes['link-to-detail']}>
         <img className={classes['img-template']} src={src} alt={alt} {...rest} />
+        <div className={classes['overlay']}>
+          <span> preview template</span>
+        </div>
       </Link>
+      <div className={classes['title-template']}>
+        <h2>{title}</h2>
+      </div>
     </div>
   );
 }
